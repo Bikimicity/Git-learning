b34399c9c09bb109d18fda90bd48036d77db8a2a
HeeRyeong Kim <hr6594@naver.com>
2024-05-16 00:27:11 +0900

    5

--- 1.py
+++ 1.py
@@ -2,11 +2,3 @@ print("hi")
 print("hello")
 print("good bye")
 print("today")
-<<<<<<< HEAD
-print("master")
-print("master2")
-print("master3")
-=======
-print("main")
-print("main2")
->>>>>>> main
